0bcc6b0d8ed5144150f90f578a90c558419349d1
Hugues Malphettes <hmalphettes@gmail.com>
2012-10-04 17:53:28 +0800

    Upgrade to pax-exam-2.6

--- jetty-osgi/test-jetty-osgi/src/test/java/org/eclipse/jetty/osgi/test/TestJettyOSGiBootCore.java
+++ jetty-osgi/test-jetty-osgi/src/test/java/org/eclipse/jetty/osgi/test/TestJettyOSGiBootCore.java
@@ -1,25 +1,9 @@
-//
-//  ========================================================================
-//  Copyright (c) 1995-2012 Mort Bay Consulting Pty. Ltd.
-//  ------------------------------------------------------------------------
-//  All rights reserved. This program and the accompanying materials
-//  are made available under the terms of the Eclipse Public License v1.0
-//  and Apache License v2.0 which accompanies this distribution.
-//
-//      The Eclipse Public License is available at
-//      http://www.eclipse.org/legal/epl-v10.html
-//
-//      The Apache License v2.0 is available at
-//      http://www.opensource.org/licenses/apache2.0.php
-//
-//  You may elect to redistribute this code under either of these licenses.
-//  ========================================================================
-//
-
-package org.eclipse.jetty.osgi.boot;
+package org.eclipse.jetty.osgi.test;
 
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.profile;
+import static org.ops4j.pax.exam.spi.PaxExamRuntime.createContainer;
+import static org.ops4j.pax.exam.spi.PaxExamRuntime.createTestSystem;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,59 +13,68 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import junit.framework.Assert;
-
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.HttpContentResponse;
 import org.eclipse.jetty.client.api.Response;
 import org.eclipse.jetty.http.HttpStatus;
-import org.eclipse.jetty.osgi.boot.internal.serverfactory.DefaultJettyAtJettyHomeHelper;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.CoreOptions;
-import org.ops4j.pax.exam.Inject;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.container.def.PaxRunnerOptions;
+import org.ops4j.pax.exam.TimeoutException;
 import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.options.libraries.JUnitBundlesOption;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+//import org.eclipse.jetty.osgi.boot.internal.serverfactory.DefaultJettyAtJettyHomeHelper;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.launch.Framework;
 import org.osgi.service.http.HttpService;
 
-/**
- * Pax-Exam to make sure the jetty-osgi-boot can be started along with the httpservice web-bundle.
- * Then make sure we can deploy an OSGi service on the top of this.
- */
 @RunWith( JUnit4TestRunner.class )
-public class TestJettyOSGiBootCore
-{
-    /**
-     * Jetty-osgi including webapp support and also jetty-client.
-     * Sets the system property jetty.home.bunde=org.eclipse.jetty.osgi.boot
-     * to use the jetty server configuration embedded in 
-     * 
-     * @return list of options
-     */
+//@ExamReactorStrategy( AllConfinedStagedReactorFactory.class )
+public class TestJettyOSGiBootCore {
+
+    @Inject
+    private BundleContext bundleContext;
+
+    @Configuration
+    public Option[] config()
+    {
+        ArrayList<Option> options = new ArrayList<Option>();
+        options.addAll(provisionCoreJetty());
+        options.add(CoreOptions.junitBundles());
+        options.addAll(httpServiceJetty());
+        return options.toArray(new Option[options.size()]);
+    }
+    
     public static List<Option> provisionCoreJetty()
     {
     	List<Option> res = new ArrayList<Option>();
     	// get the jetty home config from the osgi boot bundle.
-    	res.add(PaxRunnerOptions.vmOptions("-Djetty.port=9876 -D" + DefaultJettyAtJettyHomeHelper.SYS_PROP_JETTY_HOME_BUNDLE + "=org.eclipse.jetty.osgi.boot"));
+    	res.add(CoreOptions.systemProperty("jetty.port").value("9876"));
+    	res.add(CoreOptions.systemProperty("jetty.home.bundle").value("org.eclipse.jetty.osgi.boot"));
     	res.addAll(coreJettyDependencies());
     	return res;
     }
+
     
     public static List<Option> coreJettyDependencies() {
     	List<Option> res = new ArrayList<Option>();
+
+        res.add(mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot" ).versionAsInProject().start());
+
     	res.add(mavenBundle().groupId( "org.eclipse.jetty.orbit" ).artifactId( "javax.servlet" ).versionAsInProject().noStart());
-    	res.add(mavenBundle().groupId( "org.eclipse.osgi" ).artifactId( "org.eclipse.osgi.services" ).versionAsInProject().noStart());
     	res.add(mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-deploy" ).versionAsInProject().noStart());
     	res.add(mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-server" ).versionAsInProject().noStart());  
     	res.add(mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-servlet" ).versionAsInProject().noStart());  
@@ -101,36 +94,13 @@ public class TestJettyOSGiBootCore
     	return res;
     }
     
-    public static List<Option> websocketDependencies() {
+    public static List<Option> httpServiceJetty()
+    {
     	List<Option> res = new ArrayList<Option>();
+        res.add(mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-httpservice" ).versionAsInProject().start());
+        res.add(mavenBundle().groupId( "org.eclipse.equinox.http" ).artifactId( "servlet" ).version("1.0.0-v20070606").start());
         return res;
     }
-    
-    @Inject
-    BundleContext bundleContext = null;
-
-
-    @Configuration
-    public static Option[] configure()
-    {
-        ArrayList<Option> options = new ArrayList<Option>();
-        options.addAll(provisionCoreJetty());
-        options.addAll(Arrays.asList(options(
-            // install log service using pax runners profile abstraction (there are more profiles, like DS)
-            //logProfile(),
-            // this is how you set the default log level when using pax logging (logProfile)
-            CoreOptions.systemProperty( "org.ops4j.pax.logging.DefaultServiceLog.level" ).value( "INFO" ),
-            
-        //	CoreOptions.equinox(), CoreOptions.felix(),//.version("3.0.0"),
-        		
-            mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot" ).versionAsInProject().start(),
-            mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-httpservice" ).versionAsInProject().start(),
-            
-            mavenBundle().groupId( "org.eclipse.equinox.http" ).artifactId( "servlet" ).versionAsInProject().start()
-        )));
-        return options.toArray(new Option[options.size()]);
-    }
-
     /**
      * You will get a list of bundles installed by default
      * plus your testcase, wrapped into a bundle called pax-exam-probe
@@ -146,7 +116,6 @@ public class TestJettyOSGiBootCore
         for( Bundle b : bundleContext.getBundles() )
         {
             bundlesIndexedBySymbolicName.put(b.getSymbolicName(), b);
-            //System.err.println("got " + b.getSymbolicName());
         }
         Bundle osgiBoot = bundlesIndexedBySymbolicName.get("org.eclipse.jetty.osgi.boot");
         Assert.assertNotNull("Could not find the org.eclipse.jetty.osgi.boot bundle", osgiBoot);
@@ -193,5 +162,6 @@ public class TestJettyOSGiBootCore
             client.stop();
         }
     }
-    
+    	
+	
 }
